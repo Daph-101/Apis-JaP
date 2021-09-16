1b61de1edc36cd29eceb3d749577e0f445c96d55
Daph <daphnerodriguez169@gmail.com>
2021-09-16 14:58:16 -0300

    j

--- urls.js
+++ urls.js
@@ -2,30 +2,44 @@ var URL_C_Onix ='https://daph-101.github.io/Apis-JaP/C_Onix.json'
 var URL_Fiat = 'https://daph-101.github.io/Apis-JaP/Fiat.json'
 var URL_Peugeot = 'https://daph-101.github.io/Apis-JaP/Peugeot.json'
 var URL_Suzuki = 'https://daph-101.github.io/Apis-JaP/Suzuki.json'
-
+var url;
 function autos(){
 
-  fetch(URL_C_Onix)
+  fetch(URL_Fiat)
   .then(req => req.json())
   .then(MostrarAutos =>{
+    
     MostrarAutos.forEach(element => {
-      let auto = " ";
-      let images = " ";
-      auto= `<ul>
       
+      let auto = " ";
+
+      auto= `<div>
       <a href="info-autos.html"><button id="info-auto">Ver más</button></a> <br>
       <p>` + `<strong>Marca: </strong>` + element.name +`</p>
       <p>` + `<strong>Descripcion: </strong>`+ element.description + `</p>
       <p>` + `<strong>Precio: </strong>` + element.cost + `</p>
-  </ul>`
+  </div>`
       
-  
-  images += `  <img class="img" src="img/${element.name}" style="width:350px;">` + `</p>`;
+ 
       document.getElementById("informacion").innerHTML += auto;
-      document.getElementById("imagenes").innerHTML = images;
+     
     });
-    
+    MostrarImg(url);
   })
 
 }
+function MostrarImg(url){
+ url = [URL_C_Onix, URL_Fiat];
 
+  let linea = "<br><hr><br>";
+  let Img = "";
+  Img += `
+  ` + linea +`
+  <img src="img/car1.jpg" style="width:130px;">
+    <img src="img/cat1.jpg" style="width:120px;">
+    <img src="img/car3.jpg" style="width:150px;">
+ 
+  `;
+  document.getElementById("imagenes").innerHTML = Img;
+  
+}
